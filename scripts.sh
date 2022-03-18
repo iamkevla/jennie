ea1f6ca36993ea2118f66ecb6f919eabf9e85d0f
iamkevla <kevin.vlahos@gmail.com>
2022-03-18 20:13:41 +1100

    more

--- scripts.sh
+++ scripts.sh
@@ -74,7 +74,9 @@ docker rm pci_jenkins_1
 docker run -d --name pci_jenkins_1 \
   --privileged \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker \
   -v pci_jenkins_home_1:/var/jenkins_home \
+  -v /etc/localtime:/etc/localtime:ro \
   -p 8081:8080 kevla/jenkins
 
 
